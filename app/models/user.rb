3ec4bfa809b46e61397895cab4249afdc7659b67
Ken <kvaczi2@gmail.com>
2013-03-08 19:23:54 -0600

    setup mailer and protected ENV Variable initializers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable,  :timeoutable 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable, :omniauthable
+         :confirmable, :lockable#, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
