164db2d1ddcc33300b113786fa562c3795d109f7
Ken <kvaczi2@gmail.com>
2013-04-15 18:38:55 -0500

    changed redirect to back_path after registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,24 @@ class ApplicationController < ActionController::Base
   end
   helper_method :bot_user?
   
+  # do this after a user signs in
+  #TODO: implement banned funcationality for users
+  def after_sign_in_path_for(resource) 
+    if resource.is_a?(User)
+      if false # resource.banned?
+        sign_out resource
+        flash[:error]  = "This account has been suspended..."
+        flash[:notice] = nil # erase any notice so that error can be displayed
+        root_path
+      else
+        flash[:notice] = "Welcome back!"
+        back_path
+      end
+    else
+      super
+    end
+  end
+  
   # sets the current page as the back path for following pages to return to when back_path is redirected to
   def set_back_path
     unless @new_back_path_queue.present?                                                                              # prevents this from breaking if called multiple times during one request
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,24 +2,6 @@ class Users::SessionsController < Devise::SessionsController
 
   include Mobylette::RespondToMobileRequests  
   
-  # do this after a user signs in
-  #TODO: implement banned funcationality for users
-  def after_sign_in_path_for(resource) 
-    if resource.is_a?(User)
-      if false # resource.banned?
-        sign_out resource
-        flash[:error]  = "This account has been suspended..."
-        flash[:notice] = nil # erase any notice so that error can be displayed
-        root_path
-      else
-        flash[:notice] = "Welcome back!"
-        back_path
-      end
-    else
-      super
-    end
-  end
-  
   # devise redirect after sign out
   def after_sign_out_path_for(resource)
     super
