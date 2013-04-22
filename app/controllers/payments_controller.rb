4c57a4f72caaa555f39f66ee3d34e25c3dee371b
Ken <kvaczi2@gmail.com>
2013-04-21 19:22:16 -0500

    testing flash messages

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,7 +6,8 @@ class PaymentsController < ApplicationController
   def new
     @payment = Payment.new(params[:payment])
     @payment.fund_id ||= receiving_fund.id
-    @payment.amount = 5
+    @payment.amount = 500
+    flash[:notice] = 'hi this is a flash notice message that is really long ldkaslkfjlskdj'
     respond_to do |format|
       format.html { }
       format.mobile { }
