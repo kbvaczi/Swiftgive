517588135ebb0663fd4313e9aea0e6f3e0d39e48
Ken <kvaczi2@gmail.com>
2013-04-23 21:09:21 -0500

    tweaking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < ActionController::Base
   # sets the current page as the back path for following pages to return to when back_path is redirected to
   def set_back_path
     unless @new_back_path_queue.present?                                                                              # prevents this from breaking if called multiple times during one request
-      current_back_path_queue = session[:back_path] || [root_path]
+      current_back_path_queue = session[:back_path] || [root_url]
       @new_back_path_queue    = current_back_path_queue.dup                                                           # create a copy so the two aren't linked to the same memory address
       if current_back_path_queue.include?(current_path)                                                               # if we are revisiting an old link in the queue, let's clean up the queue (prevents infinite loops)
         @new_back_path_queue.pop(@new_back_path_queue.length - @new_back_path_queue.index(current_path))              # remove everyting in the queue visited after this link in the queue
@@ -53,23 +53,25 @@ class ApplicationController < ActionController::Base
   # returns to the url where set_back_path has last been set and clears back_path
   def back_path
     unless @back_path.present?
-      current_back_path_queue = @new_back_path_queue || session[:back_path] || [root_path]                      
+      current_back_path_queue = @new_back_path_queue || session[:back_path] || [root_url]                      
       if current_back_path_queue.include?(current_path)
         if current_back_path_queue.length > 1
-          @back_path = current_back_path_queue[current_back_path_queue.index(current_path) - 1] || root_path  
+          @back_path = current_back_path_queue[current_back_path_queue.index(current_path) - 1] || root_url  
         else
-          @back_path = root_path
+          @back_path = root_url
         end
       else
-        @back_path = current_back_path_queue.last || root_path
+        @back_path = current_back_path_queue.last || root_url
       end
     end
     @back_path
   end
   helper_method :back_path
   
-  def current_path
-    @current_path ||= url_for(params.merge(:authenticity_token => nil, :utf8 => nil, :sort => nil, :sort_order => nil))
+  def current_path(options = {})
+    options = request.params.symbolize_keys.merge(options)
+    @current_path ||= url_for Rails.application.routes.recognize_path(request.path).merge(options)
+    #@current_path ||= url_for(params.merge(:authenticity_token => nil, :utf8 => nil, :sort => nil, :sort_order => nil))
   end
   helper_method :current_path
   
