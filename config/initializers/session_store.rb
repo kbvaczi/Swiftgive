759ef0c2397097d5761b772d9e63fdeb353e7fab
Ken <kvaczi2@gmail.com>
2014-05-24 07:11:16 -0500

    added standard slogan in en.yml, changed session store to cookie store from memcache store.  Memcache was set for more sucurity while using balanced payments, cookie store is less resource intensive and is fine for square cash

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
 # Use cookie-based session storage
-# Swiftgive::Application.config.session_store :cookie_store, key: '_swiftgive_session'
+Swiftgive::Application.config.session_store :cookie_store, key: '_swiftgive_session', expire_after: 1.week
 
 # Use memcache session storage
-Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 20.minutes
+# Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 20.minutes
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
