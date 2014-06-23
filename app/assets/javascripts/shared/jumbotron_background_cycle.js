0e22c1ba7376369481e44c6aa69e7ab27a515772
Ken <kvaczi2@gmail.com>
2014-06-22 19:09:08 -0500

    fixed bug in carosel that caused lockup

--- app/assets/javascripts/shared/jumbotron_background_cycle.js
+++ app/assets/javascripts/shared/jumbotron_background_cycle.js
@@ -5,17 +5,16 @@ $(document).ready(function(){
 function cyclePageBackgroundImage (){
 	var numberOfBackgrounds = 3;
 	var target = $('.page-background-image');
-	var currentBackgroundNumber = parseInt(target.data('background-number')) || 1;	
-    if (currentBackgroundNumber >= numberOfBackgrounds) {
-    	var nextBackgroundNumber = 1;
-    } else {
-    	var nextBackgroundNumber = (currentBackgroundNumber + 1);
-	}
-	//preload next background early so it is downloaded in time before visible	   
-	target.before("<div class='page-background-image background-image-" + nextBackgroundNumber + "' data-background-number='" + nextBackgroundNumber + "'></div>");
+	var currentBackgroundNumber = parseInt(target.data('background-number')) || 1;
 	window.setTimeout ( function() {
-		target.fadeOut(2000, function() { $(this).remove(); });	
-		cyclePageBackgroundImage();
-	}, 10000 );		    
+	    if (currentBackgroundNumber >= numberOfBackgrounds) {
+	    	var nextBackgroundNumber = 1;
+	    } else {
+	    	var nextBackgroundNumber = (currentBackgroundNumber + 1);
+		}		
+		target.before("<div class='page-background-image background-image-" + nextBackgroundNumber + "' data-background-number='" + nextBackgroundNumber + "'></div>");							
+		target.fadeOut(3000, function() { $(this).remove(); });
+	    cyclePageBackgroundImage();
+    }, 10000 );   
 }
 
