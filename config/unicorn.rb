c756deddcee0e1aeb4712774e8e9d071eb7dd22f
Ken <kvaczi2@gmail.com>
2014-06-29 13:16:15 -0500

    sidekiq worker in unicorn process now for all environments

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -22,12 +22,9 @@ before_fork do |server, worker|
     Rails.logger.info('Disconnected from ActiveRecord')    
   end
 
-  # Assign one sidekick worker on the dyno in addition to unicorn processes, we will have a dedicated worker on production
-  if Rails.env.development? or Rails.env.staging?
-    unless @sidekiq_pid.present?
-      Rails.logger.info 'Starting Sidekiq Process'
-      @sidekiq_pid ||= spawn("bundle exec sidekiq -C ./config/sidekiq.yml")
-    end
+  unless @sidekiq_pid.present?
+    Rails.logger.info 'Starting Sidekiq Process'
+    @sidekiq_pid ||= spawn("bundle exec sidekiq -C ./config/sidekiq.yml")
   end
 
 end
